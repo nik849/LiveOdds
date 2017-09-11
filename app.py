0af05952e50c618634d40a91250fa023ede13bb0
Nick Hale <nickhale@protonmail.ch>
2017-09-11 14:35:42 +0100

    Fixed issues

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from liveodds.api import totalcorner
 from liveodds.config import totalcorner_test_token
 from liveodds.processing import process
 
-
 app = Flask(__name__)
 app.secret_key = 'key'
 
@@ -45,7 +44,7 @@ def submit():
     print(data)
 
     results = process(data)
-    return render_template('/fulltime.html' **results)
+    return render_template('/fulltime.html', **results)
 
 
 if __name__ == "__main__":

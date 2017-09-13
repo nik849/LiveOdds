9f2607522a6a6a53657eecbc89dafbb28276bc7d
Nick Hale <nickhale@protonmail.ch>
2017-09-13 13:35:50 +0100

    Started Calcs

--- liveodds/processing.py
+++ liveodds/processing.py
@@ -9,10 +9,22 @@ def process(data, tc_data, leagues):
 
     unwanted_keys = ['h_id', 'a_id', 'hc', 'ac', 'hg', 'ag', 'hrc', 'arc',
                      'hyc', 'ayc', 'hf_hc', 'hf_ac', 'hf_hg', 'hf_ag', 'ish',
-                     'hp', 'ap']
+                     'hp', 'ap', 'asian_corner']
+
     for match in tc_data["matches"]:
         if match["status"] == 'full':
             results.update(match)
+            calcs = {}
+            calcs["Datk"] = len(match["attacks_h"]) - len(match["attacks"])\
+                / int(data["Min"])
+            calcs["GttpH"] = len(match["shot_on_h"]) * float(data["Ptph"])
+            calcs["GttfH"] = len(match["shot_on_h"]) * float(data["Ptfh"])
+            calcs["GttpA"] = len(match["shot_on"]) * float(data["Ptpa"])
+            calcs["GttfA"] = len(match["shot_on"]) * float(data["Ptfa"])
+            calcs["GtatpH"] = len(match["attacks_h"]) * float(data["PAtpH"])
+            calcs["GtapA"] = len(match["attacks"]) * float(data["Patpa"])
+            results.update(calcs)
+
         elif match["status"] == data["Min"]:
             results_preds.update({'Minute': data["Min"]})
             results_preds.update(match)
